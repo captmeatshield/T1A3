61f0c1b5259a9922932851eeebdf45653ca4ac5b
david <dcoorloff@gmail.com>
2022-09-19 21:24:11 +1000

    exercises added

---
+++ src/exercises.py
@@ -0,0 +1,102 @@
+#Front Delt, Mid Delt, Rear Delt.
+shoulders = [
+    'Barbell Overhead Press',
+    'Landmine Press',
+    'Arnold Press',
+    'Dumbbell Lateral raise',
+    'Cable Lateral raise',
+    'Reverse Fly',
+    'Front Raise',
+    'Rear Delt Row'
+    'Z Press',
+    'Handstand Pushup'
+]
+
+# Tricep Longhead, Tricep Lateral Head, Medial Head, Bicep Longhead, Bicep Shorthead, brachialis
+arms = [
+    'Tricep Pushdown',
+    'Diamond Cutter Pushup',
+    'Overhead Rope Extension',
+    'Upright Dip',
+    'Close Grip Bench Press',
+    'Lying Tricep Extension',
+    'Preacher Curl',
+    'Cable Curl',
+    'Spider Curl',
+    'Waiters Curl',
+    'Inclined Dumbbell Curl',
+    'Chinup',
+    'Barbell Curl',
+    'Dumbbell Curl',
+]
+
+#Upper chest, Lower Chest, Mid Chest, Cross midline
+chest = [
+    'Bench Fly',
+    'Pushup',
+    'Floor Fly',
+    'Underhand Dumbbell Bench Press',
+    'Cable Crossover',
+    'Dip',
+    'Weighted Dip',
+    'Inclined Bench Press',
+    'Dumbbell Bench Press',
+    'Barbell Bench Press',
+    'One Arm Crossover',
+]
+
+#Lats, Upper Traps, Lower Traps, Lower Back, Rotator Cuff, Teres Major, Rhomboids
+back = [
+    'Dead Lift',
+    'Pendlay Row',
+    'Tripod Row',
+    'Straight Arm Pushdown',
+    'Dumbbell Pullover',
+    'Medows Row',
+    'High Cable Row',
+    'Lat Row',
+    'Dead Row',
+    'Weighted Pullup',
+    'Pullup',
+    'Barbell Row',
+    'Hyper Extension',
+]
+
+#Vastus Medialias Quad, Vastus Lateralis Quad, Vastus intermedius Quad, Rectus Femerus Quad, Lateral Hamstring, Medial Hamstring, Glute medius, Glute maximus, calfs.
+legs = [
+    'Dead Lift' 
+    'Squats',
+    'Glute Ham Raise',
+    'Barbell Hip Thrust',
+    'Bulgarian Spit Squat',
+    'Front Lunge',
+    'Walking Lunge',
+    'Side Lunge',
+    'Back Lunge',
+    'Barbell Backwards Lunge',
+    'Leg Press',
+    'Leg Extension',
+    'Cuff Kickback',
+    'Romanian Dead Lift',
+    'Single Leg RDL',
+    'Hamstring Curl',
+    'Banded Step Through',
+    'Kettlebell Swing',
+    'Cable Pullthrough',
+    'Dumbbell Step Through',
+    'Standing Calf Raise',
+    'Seated Calf Raise',
+]
+
+corrective = {
+    'Face Pull',
+    'Rotator Cuff Cable Pull',
+    'Rotator Cuff Dumbbell Pull',
+    'Crab Walks',
+    'Angels and Devils',
+
+
+}
+
+
+def shoulders(self):
---
+++ src/main.py
@@ -0,0 +1,28 @@
+#Requirement
+#Implement features in the software development plan you have designed. You must utilise a range of programming concepts and structures using Python such as:
+# - variables and variable scope
+# - loops and conditional control structures
+# - write and utilise simple functions
+# - error handling
+# - input and output
+# - importing a Python package
+# - using functions from a Python package
+# R12	Apply DRY (Don’t Repeat Yourself) coding principles to all code produced.
+# R13	Apply all style and conventions for the programming language consistently to all code produced.
+# R14	Creates an application which runs without error and has features that are consistent with the development plan.
+# R15	Design TWO tests which check that the application is running as expected.
+
+# Each test should:
+# - cover a different feature of the application
+# - state what is being tested
+# - provide at least TWO test cases and the expected results for each test case
+
+# > An outline of the testing procedure and cases should be included with the source code of the application
+# R16	Utilise source control throughout the development of the application by:
+# - making regular commits (at least 20 commits) with a commit message that summarises the changes
+# - pushing all commits to a remote repository
+# R17	Utilise developer tools to facilitate the execution of the application:
+# For example,
+# - writing a script which turns the application into an executable
+
+
